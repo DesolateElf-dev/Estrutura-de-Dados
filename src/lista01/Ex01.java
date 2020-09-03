2f2e9c4ee7851eba701eb2c52f415f7509fe80cb
Eduardo Maciel Sanchez <54561697+DesolateElf-dev@users.noreply.github.com>
2020-09-03 13:17:31 -0300

    Update

--- src/lista01/Ex01.java
+++ src/lista01/Ex01.java
@@ -1,9 +1,16 @@
 package lista01;
 
+/*
+1- Carregar um vetor [100] real de valores de saldos. Calcular e exibir:
+- Média dos saldos positivos entre 100 à 1000
+- Média geral dos saldos
+- Soma dos saldos negativos
+ */
+
 public class Ex01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
