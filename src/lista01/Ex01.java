d1dfe34fc723646c0b2f85946a1e92b976fc9091
Eduardo Maciel Sanchez <54561697+DesolateElf-dev@users.noreply.github.com>
2020-09-04 18:11:29 -0300

    Update

--- src/lista01/Ex01.java
+++ src/lista01/Ex01.java
@@ -10,8 +10,37 @@ package lista01;
 public class Ex01 {
 
 	public static void main(String[] args) {
+		double soma=0;
+		double soma2=0;
+		int cont=0;
+		double med=0;
+		double med2=0;
 		
-
+		double saldos[] = new double[100];	
+		
+		geraValor(saldos);
+		for(int i=0; i<100;i++) {
+			if (saldos[i]>=100 && saldos[i]<=1000){
+				soma+=saldos[i];
+				cont++;
+				med = soma/cont;
+			}
+			med2 += saldos[i];
+			if (saldos[i]<0) {
+				soma2+=saldos[i];
+			}
+		}
+		System.out.printf("A média entre 100 e 1000 é: R$ %.2f %n", med);
+		System.out.printf("A média geral é: R$ %.2f %n", med2);
+		System.out.printf("A soma dos saldos negativos é: R$ %.2f %n", soma2);
+		
+	}
+	
+	public static void geraValor(double[] saldos) {
+		for (int i=0;i<100;i++) {
+			double valor = (Math.random()*10000)-5000;
+			saldos[i] = valor;			
+		}
 	}
 
 }
--- src/lista01/Ex02.java
+++ src/lista01/Ex02.java
@@ -1,15 +1,41 @@
 package lista01;
 /*
 2-  Carregar um vetor [5] inteiro. Enviar cada elemento para uma função e esta 
-irá retornar o seu fatorialque será armazenado em um outro vetor. 
+irá retornar o seu fatorial que será armazenado em um outro vetor. 
 Exibir os dados dos vetores
 */
 
 public class Ex02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int vetorInt[] = new int[5];
+		int vetorFat[] = new int[5];
+		
+		geraValor(vetorInt);
+		
+		for(int i=0;i<5;i++) {
+			vetorFat[i] = calcFat(vetorInt[i]);
+		}
+		for(int i=0;i<5;i++) {
+			System.out.println(i+" = "+vetorInt[i]+"! = "+vetorFat[i]);
+		}
+	}
+	
+	private static int calcFat(int fatorial) {
+	
+		if (fatorial==0) {
+			return 1;
+		}
+		else {
+			fatorial = fatorial*calcFat(fatorial-1);
+			return fatorial;
+		}				
 	}
 
+	private static void geraValor(int[] vetorInt) {
+		for (int i=0;i<5;i++) {
+			int numero = (int)(Math.random()*10);
+			vetorInt[i] = numero;
+		}
+	}
 }
