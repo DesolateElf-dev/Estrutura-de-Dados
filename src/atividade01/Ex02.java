5af9d05fc4ffbe0c122ada403d34b60597af876b
Eduardo Maciel Sanchez <54561697+DesolateElf-dev@users.noreply.github.com>
2020-09-17 17:05:32 -0300

    Update

---
+++ src/atividade01/Ex02.java
@@ -0,0 +1,28 @@
+package atividade01;
+
+/*
+2. Implemente uma Lista de temperaturas em alocação dinâmica com todas as
+operações indicadas a seguir (1,0 pontos):
+- verificar se a lista está vazia, retornando true se estiver vazia e false se nãoestiver;
+- adicionar uma temperatura no início da lista, caso a operação não possa ser realizada,
+mostre mensagem avisando;
+- adicionar uma temperatura no final da lista, caso a operação não possa ser realizada,
+mostre mensagem avisando;
+- adicionar uma temperatura em determinada posição da lista, caso a operação não
+possa ser realizada, mostre mensagem avisando;
+- remover a temperatura do início da lista, retornando o valor que foi removido, caso a
+operação não possa ser realizada, mostre mensagem avisando;
+- remover a temperatura do final da lista, retornando o valor que foi removido, caso a
+operação não possa ser realizada, mostre mensagem avisando;
+- remover a temperatura de determinada posição da lista, retornando o valor que foi
+removido, caso a operação não possa ser realizada, mostre mensagem avisando;
+- percorrer e apresentar cada um dos elementos da lista
+ */
+
+public class Ex02 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/atividade01/Ex03.java
@@ -0,0 +1,28 @@
+package atividade01;
+
+/*
+3. A classe Aluno possuí atributos que são criados na classe concreta e acessados por
+meio dos métodos públicos da classe (getters e setters). Dado o cenário implemente
+uma Lista Encadeada (4,5 pontos)
+Sejam criados em uma classe os atributos RA de tipo de dado inteiro, nome de
+tipo de dado String, turma do tipo de dado String e semestre do tipo de dado
+String. Estes atributos devem ser acessados por meio de seus métodos públicos
+(getters e setters).
+- Implemente um método que adicione um aluno no final da lista
+- Implemente um método que adicione um aluno no início da lista
+- Implemente um método que adicione um aluno no meio da lista
+- Implemente um método que remova um aluno no final da lista
+- Implemente um método que remova um aluno no início da lista
+- Implemente um método que remova um aluno no meio da lista
+- Implemente um método que apresente cada um dos elementos da lista
+- Realize pelo menos 5 simulações para cada uma das operações
+*/
+
+public class Ex03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
